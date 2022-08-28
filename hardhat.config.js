5a5557f85c3d552d1340f06d3c8e5925ff58fd3e
InventedMost <netvid00@gmail.com>
2022-08-28 09:50:00 +0100

    added goerli network in hardhet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
+  console.log(`Deploying Contract`);
   const Storage = await hre.ethers.getContractFactory("SimpleStorage");
   const storage = await Storage.deploy();
   await storage.deployed();
+  console.log(`Deployed Contract to ${storage.address}`);
 }
 
 main()
